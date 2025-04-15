66ecf0bd053b6d3edfa4c42a7eb97fb854c8fbd4
REGERA <ilyass.med.fkh@gmail.com>
2025-04-16 00:08:39 +0100

    adding server side and SPA and structure

---
+++ src/about.js
@@ -0,0 +1,10 @@
+export default () => {
+    return (
+        `
+        <nav>
+            <a href="/" data-link>Home</a>
+            <a href="/about" data-link>About</a>
+        </nav>
+        <h1>About Page</h1>`
+    )
+}
