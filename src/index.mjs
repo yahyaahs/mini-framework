ca2f0c65620fae8e018b3f303877da01ca195b1c
REGERA <ilyass.med.fkh@gmail.com>
2025-04-17 23:19:04 +0100

    finding new way to setupe listner and useState

--- src/index.mjs
+++ src/index.mjs
@@ -12,15 +12,28 @@ export default () => {
             createElement('button', {
                 style: "width: 100px; heigth: 20px; border: 2px solid black;",
                 onClick: function (state, setState) {
-                    console.log('boobled');
+                    console.log('boobled: ', state('count'));
                     setState('count', (state('count') || 0) + 1);
                 }
             }, ['Increment']),
-            createElement('input', { type: 'text', style: 'margin-top: 10px;' })
+            createElement('p', { id: 'change_input' }, ['You are Typing: ']),
+            createElement('input', {
+                style: 'margin-top: 10px;',
+                value: '',
+                onKeydown: function (state, setState, e) {
+                    console.log('input event fired', state('change_input'));
+                    setState('change_input', (state('change_input') || '') + e.target.value);
+                }
+            })
         ])
     );
 
     app.state.set('count', 0);
+    app.state.set('change_input', '');
+    // app.eventRegistry.set('gg', () => {
+    // const input = document.querySelector('.nigga')
+    // return input.value
+    // })
 
     return app;
 }
