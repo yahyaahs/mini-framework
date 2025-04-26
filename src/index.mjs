6e24271203082140e58a60f90f4f93150a62b157
REGERA <ilyass.med.fkh@gmail.com>
2025-04-26 18:05:08 +0100

    making updater for classes

--- src/index.mjs
+++ src/index.mjs
@@ -1,6 +1,5 @@
 import { Link } from "../lib/hooks.mjs";
 import { createComponent } from "../lib/virtual_dom.mjs"
-import { createElement } from "../lib/virtual_element.mjs";
 
 export default () => {
     const home = createComponent()
@@ -19,15 +18,24 @@ export default () => {
     }
 
 
+    const breakHeart = (_, __, e) => {
+        const target = home.findElementByDom(e.target.getAttribute('key'))
+        target.toggleClass('dead')
+    }
+
     home.state.set('_heart');
-    const heart = ['span', { class: 'heart', style: 'margin-left: 10px; color: red;' }, ['<3']]
 
     const addHeart = (state, setState) => {
         const prev = state('_heart');
 
+        const newHeart = home.createElement('span', {
+            class: 'heart alive',
+            onClick: (e) => breakHeart(state, setState, e),
+        }, ['<3']);
+
         (prev === undefined) ?
-            setState('_heart', [createElement(...heart)])
-            : setState('_heart', [...prev, createElement(...heart)]);
+            setState('_heart', [newHeart])
+            : setState('_heart', [...prev, newHeart]);
     }
 
     const removeHeart = (state, setState) => {
@@ -40,22 +48,22 @@ export default () => {
     }
 
     return home.setElements(
-        createElement(...Link('/about', 'go to about')),
-        createElement(...Link('/test', 'go to test')),
-        createElement('div', { class: 'counter', style: 'margin-top: 80px; display: flex; flex-direction: column ;justify-content: center; align-items: center; height: 100%;' }, [
-            createElement('h1', { style: 'text-align: center; size: 25px;' }, ["nigga-mind Mini-frameWork"]),
-            createElement('h2', {}, ['Counter']),
-            createElement('p', { id: '_countRef', style: "color: red;" }, ['0']),
-            createElement('button', { style: "width: 100px; heigth: 20px; border: 2px solid black;", onClick: increment }, ['Increment']),
-            createElement('p', {}, ['You are Typing: ', createElement('span', { id: '_textRef', style: 'color: red;' })]),
-            createElement('input', { style: 'margin: 10px;', value: '', onKeydown: updateText }),
-            createElement('div', { class: 'new', style: 'display: flex;' }, [
-                createElement('button', {
+        home.createElement(...Link('/about', 'go to about')),
+        home.createElement(...Link('/test', 'go to test')),
+        home.createElement('div', { class: 'counter', style: 'margin-top: 80px; display: flex; flex-direction: column ;justify-content: center; align-items: center; height: 100%;' }, [
+            home.createElement('h1', { style: 'text-align: center; size: 25px;' }, ["nigga-mind Mini-frameWork"]),
+            home.createElement('h2', {}, ['Counter']),
+            home.createElement('p', { id: '_countRef', style: "color: red;" }, ['0']),
+            home.createElement('button', { style: "width: 100px; heigth: 20px; border: 2px solid black;", onClick: increment }, ['Increment']),
+            home.createElement('p', {}, ['You are Typing: ', home.createElement('span', { id: '_textRef', style: 'color: red;' })]),
+            home.createElement('input', { style: 'margin: 10px;', value: '', onKeydown: updateText }),
+            home.createElement('div', { class: 'new', style: 'display: flex;' }, [
+                home.createElement('button', {
                     style: "width: 20px; heigth: 20px; border: 2px solid black;",
                     onClick: addHeart
                 }, ['+']),
-                createElement('div', { id: '_heart', class: 'chess' }),
-                createElement('button', {
+                home.createElement('div', { id: '_heart', class: 'chess' }),
+                home.createElement('button', {
                     style: "width: 20px; heigth: 20px; border: 2px solid black;",
                     onclick: removeHeart,
                 }, ['-']),
