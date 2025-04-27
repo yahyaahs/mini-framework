d9f28f55d674e3f4b2553c1ad8f171dac39747e4
Ilyass Mohamed Foukahi <ilyass.med.fkh@gmail.com>
2025-04-27 12:20:42 +0100

    making navigation storage

--- src/test.mjs
+++ src/test.mjs
@@ -1,4 +1,13 @@
-export default ({ prev_data }) => {
-    console.log(prev_data);
+import { Redirect } from "../lib/hooks.mjs";
+import { __V_Server } from "../lib/main.mjs";
+
+export default () => {
+    const homeData = __V_Server.PrevApp.get('/')
+    if (typeof homeData === "undefined") {
+        Redirect('/')
+        return
+    }
+
+    console.log(homeData.state);
     return null
 }
