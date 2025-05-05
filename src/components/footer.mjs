1d6bdee2a5eba4fbf57aaaf1dfb3592828f59ff5
yahyahs <houssyahya@gmail.com>
2025-05-05 14:37:02 +0100

    infos

--- src/components/footer.mjs
+++ src/components/footer.mjs
@@ -1,8 +1,8 @@
 const Footer = (root) => {
     return root.createElement('footer', { class: 'info' }, [
         root.createElement('p', {}, ["Double-click to edit a todo"]),
-        root.createElement('p', {}, ["Created by the TodoMVC Team"]),
-        root.createElement('p', {}, ["Part of ", root.createElement('a', { href: "http://todomvc.com" }, ["TodoMVC"]),])
+        root.createElement('p', {}, ["Created by 0XPYR0"]),
+        root.createElement('p', {}, ["Part of ", root.createElement('a', { href: "http://learn.zone01oujda.ma" }, ["ZONE 01 OUJDA"]),])
     ])
 }
 
